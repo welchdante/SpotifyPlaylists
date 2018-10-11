5211ee1af4506c1ab8b8cacbd9c7bd0718807bc3
Julia Hildebrand <hildejul.hildebrand@gmail.com>
2018-10-11 14:13:33 -0400

    Small update to hello world

--- src/main/java/com/cis350/spotifyplaylists/GoogleMapsCollector.java
+++ src/main/java/com/cis350/spotifyplaylists/GoogleMapsCollector.java
@@ -4,7 +4,7 @@ public class GoogleMapsCollector {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
+        System.out.println("Hello, World test");
     }
 
 }
