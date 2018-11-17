63d2bbaface3dbe051bcfd0447bd57ec5a535f71
Dante Welch <welchdan@mail.gvsu.edu>
2018-11-17 13:12:19 -0500

    docs for helpers

--- src/main/java/com/cis350/spotifyplaylists/PromptHelpers.java
+++ src/main/java/com/cis350/spotifyplaylists/PromptHelpers.java
@@ -9,6 +9,8 @@ import java.util.Scanner;
  */
 public class PromptHelpers {
 
+    /** Greets the user and gives directions of how to use the program.
+     */
     public static void greeting() {
         System.out.println("Hello! Let's make a playlist for your roadtrip. \n" +
                 "First, we need to authenticate with your Spotify account.\n" +
@@ -16,6 +18,9 @@ public class PromptHelpers {
                 "have a code in it. Copy and paste that code into the program to authenticate your account.");
     }
 
+    /** Prompts the user for their desired start and end locations.
+     * @return Hash map of the starting and ending latitute and longitude of the road trip.
+     * */
     public static HashMap promptUserForCoordinates() {
         HashMap coordinates = new HashMap();
         Scanner userInput = new Scanner(System.in);
@@ -39,6 +44,9 @@ public class PromptHelpers {
         return coordinates;
     }
 
+    /** Prompts the user for the desired name of the playlist.
+     * @return String of the name of the playlist
+     * */
     public static String prompUserForPlaylistName() {
         Scanner userInput = new Scanner(System.in);
         String playlistName = userInput.next();
